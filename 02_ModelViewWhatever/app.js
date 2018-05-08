34882506eed0529c480db22895377e099dab30c5
mattskel <mattskelley@hotmail.com>
2018-05-08 19:54:24 +0800

    02_06 HTML Aside: Custom Attributes

--- 02_ModelViewWhatever/app.js
+++ 02_ModelViewWhatever/app.js
@@ -1,10 +1,12 @@
-// MODULE
-var angularApp = angular.module('angularApp', []);
+//// MODULE
+//var angularApp = angular.module('angularApp', []);
+//
+//// CONTROLLERS
+//angularApp.controller('mainController', ['$scope', function ($scope) {
+//    
+//}]);
 
-// CONTROLLERS
-angularApp.controller('mainController', ['$scope', function ($scope) {
-    
-}]);
+console.log($("h1").attr("ng-reply"));
 
 
 
