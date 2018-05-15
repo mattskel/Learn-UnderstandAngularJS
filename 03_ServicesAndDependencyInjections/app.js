8ab95601fc1546db7b6c89b904c7743b6f1f7568
mattskel <mattskelley@hotmail.com>
2018-05-15 20:11:21 +0800

    03_11 The Scope Service

--- 03_ServicesAndDependencyInjections/app.js
+++ 03_ServicesAndDependencyInjections/app.js
@@ -1,20 +1,9 @@
-//// MODULE
-//var angularApp = angular.module('angularApp', []);
-//
-//// CONTROLLERS
-//angularApp.controller('mainController', ['$scope', function ($scope) {
-//    
-//}]);
+// MODULE
+var myApp = angular.module('myApp', []);
 
-var Person = function(firstname, lastname) {
-    this.firstname = firstname;
-    this.lastname = lastname;
-}
-
-function logPerson (person) {
-    console.log(person);
-}
-
-var john = new Person('John','Doe');
-logPerson(john);
-
+// CONTROLLERS
+myApp.controller('mainController', function ($scope) {
+    $scope.name = 'Jane Doe';
+    $scope.occupation = 'Coder';
+    console.log($scope);
+});
