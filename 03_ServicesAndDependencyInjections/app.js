d4bec059992461015538df20c3b3a000dbc3ac53
mattskel <mattskelley@hotmail.com>
2018-05-16 20:12:29 +0800

    03_14 Getting Other Services

--- 03_ServicesAndDependencyInjections/app.js
+++ 03_ServicesAndDependencyInjections/app.js
@@ -1,12 +1,20 @@
 // MODULE
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
 // CONTROLLERS
-myApp.controller('mainController', function ($scope) {
+myApp.controller('mainController', function ($scope, $log, $filter, $resource) {
+//    console.log($log);
+//    $log.log('Hello');
+//    $log.info('This is info');
+//    $log.warn('Warning');
+//    $log.debug('Debug info');
+//    $log.error('ERROR');
+//    $scope.name = 'John';
+//    $scope.formattedname = $filter('uppercase')($scope.name);
+//    
+//    $log.info($scope.name);
+//    $log.info($scope.formattedname);
+    
+    console.log($resource);
 });
 
-var searchPeople = function(firstName, height, age) {
-    return 'Jane Doe';
-}
-
-console.log(searchPeople(1,2,3));
