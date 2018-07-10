4ac2a08ea63d122bab26199568c9c7ba4ea67970
mattskel <mattskelley@hotmail.com>
2018-07-10 21:06:53 +0800

    04_17 Scope and Interpolation

--- 04_DataBindingAndDirectives/app.js
+++ 04_DataBindingAndDirectives/app.js
@@ -2,7 +2,11 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', function ($scope) {
+angularApp.controller('mainController', ['$scope', '$timeout', function ($scope, $timeout) {
+  $scope.name = 'Tony';
+  $timeout(function() {
+    $scope.name = 'Everybody';
+  }, 3000);
 }]);
 
 
