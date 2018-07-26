70d9fbc538ad8b575cb95a97492365f36d1664e2
mattskel <mattskelley@hotmail.com>
2018-07-26 22:04:53 +0800

    05_27 Routing, Templates and Controllers

--- 05_SinglePageApplications/app.js
+++ 05_SinglePageApplications/app.js
@@ -1,13 +1,26 @@
 // MODULE
-var angularApp = angular.module('angularApp', []);
+var angularApp = angular.module('angularApp', ['ngRoute']);
+
+angularApp.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'pages/main.html',
+      controller: 'mainController'
+    })
+    .when('/second', {
+      templateUrl: 'pages/second.html',
+      controller: 'secondController'
+    })
+})
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', function ($scope) {
-    $scope.name = "Main";
+angularApp.controller('mainController', ['$scope', '$location', '$log', function ($scope, $location, $log) {
+//  $log.info($location.path());
+    $scope.name = 'Main';
 }]);
 
-angularApp.controller('secondController', ['$scope', function ($scope) {
-    $scope.name = "Second";
+angularApp.controller('secondController', ['$scope', '$location', '$log', function ($scope, $location, $log) {
+  $scope.name = 'Second';
 }]);
 
 
